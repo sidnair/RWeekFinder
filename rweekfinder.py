b56c37778d09d9c7f3e2e21be7c4e6c1e553b9da
Sid Nair <ssn2114@columbia.edu>
2011-02-11 02:50:45 -0500

    fixed ui gif issue, yelp integration, distance calculation

--- rweekfinder.py
+++ rweekfinder.py
@@ -7,6 +7,9 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
+"""Load custom Django template filters"""
+webapp.template.register_template_library('djangofilters')
+
 class Restaurant(db.Model):
     name = db.StringProperty()
     #description = db.StringProperty(multiline=True)
@@ -16,6 +19,9 @@ class Restaurant(db.Model):
     opentable_link = db.StringProperty()
     genre = db.StringProperty()
     neighborhood = db.StringProperty()
+    address = db.StringProperty()
+    lat = db.FloatProperty()
+    lng = db.FloatProperty()
 
 class MainPage(webapp.RequestHandler):
     def get(self):
@@ -31,7 +37,10 @@ class MainPage(webapp.RequestHandler):
             'neighborhood':'Ghetto',
             'opentable_link':querier.Querier().get_opentable_link('Perilla'),
             'yelp_link':'ylink',
-            'rating':4.0
+            'rating':4.0,
+            "lat": 40.7322680,
+            "lng": -74.0017980',
+            'address':'9 Jones Street'
             })
         '''
 class Adder(webapp.RequestHandler):
@@ -41,8 +50,8 @@ class Adder(webapp.RequestHandler):
     def addAll(self):
         ct = 0
         for name in scraper.get_all():
-           ct += RestaurantMaker().make('Perilla') 
-           if ct > 5:
+           ct += RestaurantMaker().make(name) 
+           if ct > 1:
                self.redirect('/')
                return
 
@@ -62,24 +71,53 @@ class RestaurantPostHandler(webapp.RequestHandler):
 
 class RestaurantMaker:
     def make(self, r_name):
-        r_name = 'Perilla'
         q = querier.Querier()
-        #update existing query rather than making a totally new one
         rest = db.GqlQuery("SELECT * FROM Restaurant WHERE name = :name", name = r_name).get()
-        if rest is None:
-            rest = Restaurant()
-        else:
-            return 0 
+        if rest is not None:
+            return 0
         yelp_r = q.search_yelp(r_name)
         if yelp_r is None:
             return 0
-        rest.name = r_name
-        rest.opentable_link = q.get_opentable_link(r_name)
-        rest.yelp_link = yelp_r[0]['url']
-        rest.rating = yelp_r[0]['avg_rating']
-        #for each thing in data, add the trait
-        rest.put()
+        data = {}
+        data['name'] = r_name
+        data['opentable_link'] = q.get_opentable_link(r_name)
+        data['yelp_link'] = yelp_r[0]['url']
+        data['rating'] = yelp_r[0]['avg_rating']
+        data['lat'] = yelp_r[0]['latitude']
+        data['lng'] = yelp_r[0]['longitude']
+        data['phone'] = yelp_r[0]['phone']
+        data['address'] = self.stringify(yelp_r[0], ['address1', 'address2', 'address3', 'zip'])
+        data['genre'] = self.stringify(yelp_r[0]['categories'], ['name']) 
+        data['neighborhood'] = self.stringify(yelp_r[0]['neighborhoods'], ['name'])
+        data['address'] = yelp_r[0]['address1']
+        '''
+        if yelp_r[0]['address2'] != '':
+            data['address'] += ', ' + yelp_r[0]['address2']
+        if yelp_r[0]['address3'] != '':
+            data['address'] += ', ' + yelp_r[0]['address3']
+        data['address'] += ', ' + yelp_r[0]['zip']
+        data['neighborhood'] = ''
+        for hood in yelp_r[0]['neighborhoods']:
+            data['neighborhood'] += hood['name'] + ","
+        data['genre'] = ''
+        for cat in yelp_r[0]['categories']:
+            data['genre'] = cat['name'] + ","
+        '''
+        self.add(data)
         return 1
+
+    def stringify(self, hash, keys):
+        s = []
+        if len(keys) == 1:
+            for sub in hash:
+                if sub[keys[0]] and sub[keys[0]] != '':
+                    s.append(sub[keys[0]])
+        else:
+            for key in keys:
+                if hash[key] and hash[key] != '':
+                    s.append(hash[key])
+        s.sort()
+        return ', '.join(s)
 
     def add(self, data):
         rest = db.GqlQuery("SELECT * FROM Restaurant WHERE name = :name", name = data['name']).get()
@@ -91,6 +129,9 @@ class RestaurantMaker:
         rest.rating = data['rating']
         rest.neighborhood = data['neighborhood'] 
         rest.genre = data['genre']
+        rest.lat = data['lat']
+        rest.lng = data['lng']
+        rest.address = data['address']
         rest.put()
 
 
